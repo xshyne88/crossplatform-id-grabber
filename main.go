5d134671fa0d46158c83d8607c9634cde2ddc4df
Chase Philips <chasehomedecor@gmail.com>
2020-02-18 16:32:55 -0500

    wip refactor

--- main.go
+++ main.go
@@ -6,35 +6,142 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"regexp"
 	"runtime"
 	"strings"
 
 	"github.com/denisbrodbeck/machineid"
 )
 
+const fileName = "output.txt"
+
+type Payload struct {
+	OS string
+	machineID string
+	systemUUID string
+	serialNumber string
+}
+
+const (
+	windows = "windows"
+	darwin = "darwin"
+	linux = "linux"
+)
+
 func main() {
-	_, err := os.Create("output.txt")
-	if err != nil {
-		panic(err)
+	_, err := os.Create(fileName)
+	dealWithError(err)
+
+	operatingSystem := runtime.GOOS
+	machineID = getMachineID()
+
+	payload := &Payload{
+		OS: operatingSystem
+		machineID: machineID
 	}
+
+	switch operatingSystem {
+	case windows:
+		payload.SerialNumber = windowsSerialNumber()
+		payload.systemUUID = ""
+	case darwin:
+		serialNumber, systemUUID = macInfo()
+	case linux:
+		output = linuxSerialNumber()
+		systemUUID = string(getLinuxOSSystemUUID())
+	default:
+		panic("couldn't detect os")
+	}
+
+	writeOutput(payload)
+}
+
+func windowsSerialNumber() string {
+	out, err := exec.Command("wmic", "bios", "get", "serialnumber").Output()
+	dealWithError(err)
+
+	return string(out)
+}
+
+func macInfo() []byte {
+	out, err := exec.Command("/usr/sbin/ioreg", "-l").Output()
+	findMacHardwareUUID(out)
+	dealWithError(err)
+
+	return findMacSerial(out)
+}
+
+func findMacHardwareUUID(out []byte) string, error {
+	re := regexp.MustCompile(`IOPlatformUUID\" = \"(.*)\"`)
+	matchSlice := re.FindSubmatch(out)
+	return string(matchSlice[1])
+}
+
+func findMacSerial(out []byte) []byte {
+	for _, l := range strings.Split(string(out), "\n") {
+		if strings.Contains(l, "IOPlatformSerialNumber") {
+			s := strings.Split(l, " ")
+			return []byte(s[len(s)-1])
+		}
+	}
+	panic("couldn't find IOS SerialNumber")
+}
+
+func linuxSerialNumber() []byte {
+	out, err := exec.Command("/usr/sbin/dmidecode", "-s", "system-serial-number").Output()
+	dealWithError(err)
+	return out
+}
+
+func getLinuxOSSystemUUID() []byte {
+	var response []byte
+	out, err := exec.Command("/usr/sbin/dmidecode", "-s", "system-uuid").Output()
+	if err != nil {
+		return response
+	}
+	return out
+}
+
+func createOuputFile() {
+	f, err := os.Create("output.txt")
+	return err
+
+	cwd, _ := os.Getwd()
+	if err != nil {
+		cwd = ""
+	}
+
+	fmt.Printf("File created: %s/%s\n", cwd, fileName)
+	return nil
+}
+
+func writeOutput(out []byte, machineID, systemUUID string) {
+	fileError := createOutputFile()
+	dealWithError(fileError)
+
+	macs, err := getMACAdresses()
+	dealWithError(err)
+
+	numBytes, err := f.WriteString(fmt.Sprintf("SERIAL_NUMBER=%s\nMACHINEID=%s\nHARDWARE_ADDRESSES=%v", out, machineID, macs))
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Printf("wrote %d bytes", numBytes)
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func getMachineID() string {
 	machineID, err := machineid.ID()
 	if err != nil {
 		machineID = ""
 	}
-
-	var output []byte
-
-	switch runtime.GOOS {
-	case "windows":
-		output = getWindowsOSSerial()
-	case "darwin":
-		output = getMacOSSerial()
-	case "linux":
-		output = getLinuxOSSerial()
-	default:
-		panic("couldn't detect os")
-	}
-	writeOutput(output, machineID)
+	return machineID
 }
 
 func getMACAdresses() ([]string, error) {
@@ -52,65 +159,8 @@ func getMACAdresses() ([]string, error) {
 	return as, nil
 }
 
-func getWindowsOSSerial() []byte {
-	out, err := exec.Command("wmic", "bios", "get", "serialnumber").Output()
-	if err != nil {
-		log.Fatalf("Something went wrong trying to execute the serial number command: \n %q", err)
-	}
-	return out
-}
-
-func getMacOSSerial() []byte {
-	out, err := exec.Command("/usr/sbin/ioreg", "-l").Output()
-	if err != nil {
-		log.Fatalf("Something went wrong trying to execute the serial number command: \n %q", err)
-	}
-	return findMacSerial(out)
-}
-
-func findMacSerial(out []byte) []byte {
-	for _, l := range strings.Split(string(out), "\n") {
-		if strings.Contains(l, "IOPlatformSerialNumber") {
-			s := strings.Split(l, " ")
-			return []byte(s[len(s)-1])
-		}
-	}
-	panic("couldn't find IOS SerialNumber")
-}
-
-func getLinuxOSSerial() []byte {
-	out, err := exec.Command("/usr/sbin/dmidecode", "-s", "system-serial-number").Output()
+func dealWithError(err error) {
 	if err != nil {
 		panic(err)
-	}
-	return out
-}
-
-func writeOutput(out []byte, machineID string) {
-	f, err := os.Create("output.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	cwd, _ := os.Getwd()
-	if err != nil {
-		cwd = ""
-	}
-	fmt.Printf("File created: %s/output.txt\n", cwd)
-	macs, err := getMACAdresses()
-	if err != nil {
-		panic(err)
-	}
-	numBytes, err := f.WriteString(fmt.Sprintf("SERIAL_NUMBER=%s\nMACHINEID=\"%s\"\nHARDWARE_ADDRESSES=%v", out, machineID, macs))
-	if err != nil {
-		fmt.Println(err)
-		f.Close()
-		return
-	}
-	fmt.Printf("wrote %d bytes", numBytes)
-	err = f.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
 	}
 }
